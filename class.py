d40ed6014a14799c7e3659bcfe3b91349b4796a8
Adil Khan <sohilkhan07601760@gmail.com>
2026-03-29 12:26:56 +0530

    class matrial

---
+++ class.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+# Creating object
+s1 = Student("Adil", 20)
+
+# Calling method
+s1.display()
